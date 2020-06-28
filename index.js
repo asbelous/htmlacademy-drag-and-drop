62214935dce07cd7cdbe125b1c79f65cd0d49a4b
Алексей Белоусов <arcaim@list.ru>
2020-06-28 17:22:26 +0700

    Добавил комментарии

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// Разрешаем перетаскивание элементов
 const taskListElement = document.querySelector(`.tasks__list`);
 const taskElements = taskListElement.querySelectorAll(`.tasks__item`);
-
+// Перебираем все элементы списка и присваиваем нужное значение
 for (const task of taskElements) {
     task.draggable = true;
 }
-
+// Добавляем реакцию на начало перетаскивания
 taskListElement.addEventListener(`dragstart`, (evt) => {
     evt.target.classList.add(`selected`);
 });
-
+// Добавляем реакцию на конец перетаскивания
 taskListElement.addEventListener(`dragend`, (evt) => {
     evt.target.classList.remove(`selected`);
 });
