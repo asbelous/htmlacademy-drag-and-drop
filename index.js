60670ff246a9802ed8a1651a4ec9314363b4289f
Алексей Белоусов <arcaim@list.ru>
2020-06-28 15:59:36 +0700

    Добавил реакцию на начало и конец перетаскивания

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const taskElements = taskListElement.querySelectorAll(`.tasks__item`);
 for (const task of taskElements) {
     task.draggable = true;
 }
+
+taskListElement.addEventListener(`dragstart`, (evt) => {
+    evt.target.classList.add(`selected`);
+});
+
+taskListElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+});
